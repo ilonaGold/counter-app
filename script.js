676d18455041edbe6f900cb1c321399dc09814d0
ilonaGold <songa17@inbox.lv>
2021-11-04 00:47:10 +0200

    Refactored JS using event delegation

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 const count = document.querySelector(".count");
-const btnSub = document.querySelector(".btn-subtract");
-const btnReset = document.querySelector(".btn-reset");
-const btnAdd = document.querySelector(".btn-add");
+const buttons = document.querySelector(".buttons");
 
-btnAdd.addEventListener("click", () => {
-    count.textContent++;
+buttons.addEventListener("click", (e) => {
+    if (e.target.classList.contains("btn-add")) {
+        count.textContent++;
+        setColor();
+    }
+    if (e.target.classList.contains("btn-subtract")) {
+        count.textContent--;
+        setColor();
+    }
+    if (e.target.classList.contains("btn-reset")) {
+        count.textContent = 0;
+        setColor();
+    }
 });
 
-btnSub.addEventListener("click", () => {
-    count.textContent--;
-});
-
-btnReset.addEventListener("click", () => {
-    count.textContent = 0;
-});
+function setColor() {
+    if (count.textContent > 0 ) {
+        count.style.color = "#fdc13d";
+    } else if (count.textContent < 0) {
+        count.style.color = "#df0900";
+    } else {
+        count.style.color = "#fff";
+    }
+}
